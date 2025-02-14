c318fab6938cb58d2d94e38125bf32aeefc6363d
lyric228 <xhikzerox@gmail.com>
2025-02-15 03:59:04 +0500

    Utils update
    + Rust edition 2024
    + New u64 to duration conv methods

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,49 @@
 use rand::distr::uniform::SampleUniform;
-use std::thread::sleep as std_sleep;
 use std::time::Duration;
 use rand::Rng;
 use num::Num;
 
+pub use std::thread::sleep;
 
-pub fn sleep(time: u64) {
-    std_sleep(Duration::from_millis(time));
+
+pub trait DurationUnits {
+    fn ns(self) -> Duration;
+    fn ms(self) -> Duration;
+    fn s(self) -> Duration;
+    fn m(self) -> Duration;
+    fn h(self) -> Duration;
+    fn d(self) -> Duration;
+    fn y(self) -> Duration;
+}
+
+impl DurationUnits for u64 {
+    fn ns(self) -> Duration {
+        Duration::from_nanos(self)
+    }
+
+    fn ms(self) -> Duration {
+        Duration::from_millis(self)
+    }
+    
+    fn s(self) -> Duration {
+        Duration::from_secs(self)
+    }
+    
+    fn m(self) -> Duration {
+        Duration::from_secs(self * 60)
+    }
+    
+    fn h(self) -> Duration {
+        Duration::from_secs(self * 60 * 60)
+    }
+    
+    fn d(self) -> Duration {
+        Duration::from_secs(self * 60 * 60 * 24)
+    }
+    
+    fn y(self) -> Duration {
+        Duration::from_secs(self * 60 * 60 * 24 * 365)
+    }
 }
 
 pub fn random<T>(min: T, max: T) -> T 
